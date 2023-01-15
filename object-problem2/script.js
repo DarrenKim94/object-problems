db39d594354276368f4dfd455685db7297f9422f
Darren Kim <dkim555@yahoo.com>
2023-01-14 23:11:22 -0800

    create array of people

---
+++ object-problem2/script.js
@@ -0,0 +1,24 @@
+// Create an array of people objects with first name, last name, and age, then write a 
+// JavaScript function that takes in an array and console logs the 
+// first and last names of all the people.
+
+
+
+
+const people = [
+    {
+      firstName: 'Darren',
+      lastName : 'Kim',
+      age      : 28
+    },
+    {
+      firstName: 'John',
+      lastName : 'Smith',
+      age      : 35
+    },
+    {
+      firstName: 'Jane',
+      lastName : 'Doe',
+      age      : 15
+    }
+  ]
