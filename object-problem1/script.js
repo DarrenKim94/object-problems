1698092edeccae2b03261ce86602fcb58d2666ba
Darren Kim <dkim555@yahoo.com>
2023-01-14 22:27:08 -0800

    make object

---
+++ object-problem1/script.js
@@ -0,0 +1,12 @@
+// Create a sample object, then write a JavaScript function that deletes the property of an object passed to it.
+// Your JavaScript function should take in two parameters, the object and the key/property you want to delete.
+
+let playerStats = {
+    name :'Kobe Bryant',
+    ppg   : 26,
+    apg   : 5,
+    rpg   : 5,
+    spg   : 1,
+    tpg   : 3,
+    other : ['5 rings', '1 mvp', '12 all-stars']
+}
