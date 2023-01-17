461c1e914970a4676dec50372ee1e85bda2bf301
Darren Kim <dkim555@yahoo.com>
2023-01-17 12:32:52 -0800

    write function

--- object-problem4/script.js
+++ object-problem4/script.js
@@ -1,10 +1,17 @@
 // Create an object, then write a JavaScript function 
 // that checks whether an object contains the specified key.
 
-const carBrands = {
-    Toyota : 'Tacoma',
-    Honda  : 'Civic',
-    Ford   : 'Mustang',
-    GMC    : 'Sierra',
-    Lexus  : 'GX 460'
+const fruitColors = {
+    Banana       : 'Yellow',
+    Kiwi         : 'Green',
+    Strawberry   : 'Red',
+    Orange       : 'Orange',
 }
+
+// write a function that takes in the object and key we are looking for
+function findKey(obj, key) {
+    // this method is a boolean and will return true or false
+    return obj.hasOwnProperty(key)
+}
+
+console.log(findKey(fruitColors, 'Kiwi'))
