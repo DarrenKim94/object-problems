8ebd500eabad9d4eb3845486d602e13b13e04066
Darren Kim <dkim555@yahoo.com>
2023-01-17 12:18:26 -0800

    created object

---
+++ object-problem4/script.js
@@ -0,0 +1,10 @@
+// Create an object, then write a JavaScript function 
+// that checks whether an object contains the specified key.
+
+const carBrands = {
+    Toyota : 'Tacoma',
+    Honda  : 'Civic',
+    Ford   : 'Mustang',
+    GMC    : 'Sierra',
+    Lexus  : 'GX 460'
+}
